456d98c2ef582a9b30e4cbfa8ec4bd8145094983
Dustin McDonald <dustin.m@tangentsolutions.co.za>
2019-04-20 15:12:09 +0200

    update version to 0.1

--- src/UnitTestGenerator/Properties/AddinInfo.cs
+++ src/UnitTestGenerator/Properties/AddinInfo.cs
@@ -27,7 +27,7 @@ using Mono.Addins.Description;
 [assembly: Addin(
     "UnitTestGenerator",
     Namespace = "UnitTestGenerator",
-    Version = "1.0"
+    Version = "0.1"
 )]
 
 [assembly: AddinName("UnitTestGenerator")]
